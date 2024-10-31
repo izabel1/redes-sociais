007987a559cd960de02ebd3bd504ed42a259dee3
izabel1 <129063845+izabel1@users.noreply.github.com>
2024-10-31 17:07:59 -0300

    Add files via upload

---
+++ quantidadeUsuarios.js
@@ -0,0 +1,19 @@
+async function quantidadeUsuarios() {
+    const url = https;//raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json
+    const res = await fetch(url)
+    const dados = await res.json()
+    
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+          x: nomeDasRedes,
+          y: quantidadeUsuarios,
+          type: 'bar'
+
+        }
+    ]
+}
+
+quantidadeUsuarios()
